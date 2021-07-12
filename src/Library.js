9185ff7ac8d990fa6d41bad0e24c0a65d154de77
Zach <ztollman@gmail.comm>
2021-07-11 19:23:33 -0600

    Finish library test

--- src/Library.js
+++ src/Library.js
@@ -1,7 +1,37 @@
+function createLibrary(name){
+  return {
+    name: name,
+    shelves: {
+      fantasy: [],
+      fiction: [],
+      nonFiction: [],
+    }
+  }
+}
 
+
+function addBook(library, book){
+  if (book.genre === "fantasy") {
+    return library.shelves.fantasy.push(book)
+  } else if (book.genre === "fiction") {
+    return library.shelves.fiction.push(book)
+  } else {
+    return library.shelves.nonFiction.push(book)
+  }
+}
+
+function checkoutBook(library, title, genere){
+  for (var i = 0; i < library.shelves[genere].length; i++) {
+   if (library.shelves[genere][i].title === title) {
+     library.shelves[genere].splice(i, 1);
+     return `You have now checked out ${title} from the ${library.name}`;
+   }
+ }
+ return `Sorry, there are currently no copies of ${title} available at the ${library.name}`
+}
 
 module.exports = {
-  // createLibrary,
-  // addBook,
-  // checkoutBook
+  createLibrary,
+  addBook,
+  checkoutBook
 };
